8d09cf985f9c9a5389615312db70b3d0bbc9b968
Podgorskiy <stanislav@podgorskiy.com>
2017-10-29 16:22:25 -0400

    Batch provider optimizations

--- batch_provider.py
+++ batch_provider.py
@@ -17,49 +17,91 @@
 from random import shuffle
 # import matplotlib.pyplot as plt
 from scipy import misc
-import queue
-from threading import Thread, Lock
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+from threading import Thread, Lock, Event
+import logging
 
 class BatchProvider:
     """All in memory batch provider for small datasets that fit RAM"""
     def __init__(self, batch_size, items, cycled=True, width=224, height=224):
         self.items = items
+        shuffle(self.items)
         self.batch_size = batch_size
 
-        self.current_image = 0
+        self.current_batch = 0
         self.cycled = cycled
         self.done = False
         self.image_size = (width, height)
-        self.mutex = Lock()
+        self.lock = Lock()
+        self.quit_event = Event()
 
-        self.batch = None
+        self.q = queue.Queue(16)
+        self.batches_n = len(self.items)//self.batch_size
+        logging.debug("Batches per epoch: {0}", self.batches_n)
 
     def get_batches(self):
-        t = Thread(target=self._get_batch)
-        t.start()
-        while True:
-            t.join()
-            b = self.batch
-            t = Thread(target=self._get_batch)
-            t.start()
-            yield b
+        workers = []
+        for i in range(16):
+            worker = Thread(target=self._worker)
+            worker.setDaemon(True)
+            worker.start()
+            workers.append(worker)
+        try:
+            while True:
+                yield self._get_batch()
+
+        except GeneratorExit:
+            self.quit_event.set()
+            self.done = True
+            while not self.q.empty():
+                try:
+                    self.q.get(False)
+                except queue.Empty:
+                    continue
+                self.q.task_done()
+
+    def _worker(self):
+        while not (self.quit_event.is_set() and self.done):
+            b = self.__next()
+            if b is None:
+                break
+            self.q.put(b)
 
     def _get_batch(self):
-        """Return batch generator. Batch is  a dict {"images": <images>, "labels": <labels>}"""
+        if self.q.empty() and self.done:
+            return None
+        item = self.q.get()
+        self.q.task_done()
+        return item
+
+    def __next(self):
+        self.lock.acquire()
+        if self.current_batch == self.batches_n:
+            self.done = True
+            if self.cycled:
+                self.done = False
+                self.current_batch = 0
+                shuffled = list(self.items)
+                shuffle(shuffled)
+                self.items = shuffled
+            else:
+                self.lock.release()
+                return None
+        cb = self.current_batch
+        self.current_batch += 1
+        items = self.items
+        self.lock.release()
 
         b_images = []
         b_labels = []
-        for i in range(0, self.batch_size):
-            if self.current_image == len(self.items):
-                if self.cycled:
-                    self.current_image = 0
-                    shuffle(self.items)
-                else:
-                    self.batch = None
-                    return None
-            ci = self.current_image
-            self.current_image += 1
-            image = misc.imresize(self.items[ci][1], self.image_size, interp='bilinear')
+        
+        for i in range(self.batch_size):
+            item = items[cb * self.batch_size + i]
+
+            image = misc.imresize(item[1], self.image_size, interp='bilinear')
             # Data augmentation. Should be removed from here
             # if random.random() > 0.5:
             #    im = np.fliplr(image)
@@ -67,11 +109,11 @@ class BatchProvider:
             # image = np.roll(im, random.randint(-21, 21), 1)
             # plt.imshow(image)
             # plt.show()
+
             b_images.append(image)
-            b_labels.append([self.items[ci][0]])
+            b_labels.append([item[0]])
         feed_dict = {"images": b_images, "labels": b_labels}
 
-        self.batch = feed_dict
         return feed_dict
 
 
