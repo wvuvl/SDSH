24f3ac6cb76ef272559403d591e973123939c42c
Podgorskiy <stanislav@podgorskiy.com>
2017-11-02 16:25:42 -0400

    Updated schedule

--- schedule.py
+++ schedule.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
             "total_epoch_count": 100,
             "number_of_epochs_per_decay": 150.0,
             "weight_decay_factor": 5.0e-5,
-            "learning_rate": 0.02,
+            "learning_rate": 0.01,
             "learning_rate_decay_factor": 2.0 / 3.0
         },
         {
@@ -26,40 +26,7 @@ if __name__ == '__main__':
             "total_epoch_count": 100,
             "number_of_epochs_per_decay": 150.0,
             "weight_decay_factor": 5.0e-5,
-            "learning_rate": 0.025,
-            "learning_rate_decay_factor": 2.0 / 3.0
-        },
-        {
-            "loss": "loss_spring",
-            "hash_size": 24,
-            "margin": 2.0,
-            "batch_size": 150,
-            "total_epoch_count": 100,
-            "number_of_epochs_per_decay": 150.0,
-            "weight_decay_factor": 5.0e-5,
-            "learning_rate": 0.03,
-            "learning_rate_decay_factor": 2.0 / 3.0
-        },
-        {
-            "loss": "loss_spring",
-            "hash_size": 24,
-            "margin": 2.0,
-            "batch_size": 150,
-            "total_epoch_count": 100,
-            "number_of_epochs_per_decay": 150.0,
-            "weight_decay_factor": 5.0e-5,
-            "learning_rate": 0.035,
-            "learning_rate_decay_factor": 2.0 / 3.0
-        },
-        {
-            "loss": "loss_spring",
-            "hash_size": 24,
-            "margin": 2.0,
-            "batch_size": 150,
-            "total_epoch_count": 100,
-            "number_of_epochs_per_decay": 150.0,
-            "weight_decay_factor": 5.0e-5,
-            "learning_rate": 0.04,
+            "learning_rate": 0.0125,
             "learning_rate_decay_factor": 2.0 / 3.0
         },
         {
@@ -81,7 +48,7 @@ if __name__ == '__main__':
             "total_epoch_count": 100,
             "number_of_epochs_per_decay": 150.0,
             "weight_decay_factor": 5.0e-5,
-            "learning_rate": 0.01,
+            "learning_rate": 0.0175,
             "learning_rate_decay_factor": 2.0 / 3.0
         },
         {
@@ -92,40 +59,7 @@ if __name__ == '__main__':
             "total_epoch_count": 100,
             "number_of_epochs_per_decay": 150.0,
             "weight_decay_factor": 5.0e-5,
-            "learning_rate": 0.005,
-            "learning_rate_decay_factor": 2.0 / 3.0
-        },
-        {
-            "loss": "loss_spring",
-            "hash_size": 24,
-            "margin": 2.0,
-            "batch_size": 150,
-            "total_epoch_count": 100,
-            "number_of_epochs_per_decay": 150.0,
-            "weight_decay_factor": 5.0e-5,
-            "learning_rate": 0.6,
-            "learning_rate_decay_factor": 2.0 / 3.0
-        },
-        {
-            "loss": "loss_spring",
-            "hash_size": 24,
-            "margin": 2.0,
-            "batch_size": 150,
-            "total_epoch_count": 100,
-            "number_of_epochs_per_decay": 150.0,
-            "weight_decay_factor": 5.0e-5,
-            "learning_rate": 0.001,
-            "learning_rate_decay_factor": 2.0 / 3.0
-        },
-        {
-            "loss": "loss_spring",
-            "hash_size": 24,
-            "margin": 2.0,
-            "batch_size": 150,
-            "total_epoch_count": 100,
-            "number_of_epochs_per_decay": 150.0,
-            "weight_decay_factor": 5.0e-5,
-            "learning_rate": 1.0,
+            "learning_rate": 0.02,
             "learning_rate_decay_factor": 2.0 / 3.0
         },
         # {
