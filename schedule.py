710526abd214173adf21944dd868bd8f12ecc10d
Podgorskiy <stanislav@podgorskiy.com>
2017-11-02 09:36:48 -0400

    More points for spring loss

--- schedule.py
+++ schedule.py
@@ -106,6 +106,50 @@ if __name__ == '__main__':
             "learning_rate": 0.036,
             "learning_rate_decay_factor": 2.0 / 3.0
         },
+        {
+            "loss": "loss_spring",
+            "hash_size": 24,
+            "margin": 2.0,
+            "batch_size": 150,
+            "total_epoch_count": 100,
+            "number_of_epochs_per_decay": 150.0,
+            "weight_decay_factor": 5.0e-5,
+            "learning_rate": 0.018,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "loss": "loss_spring",
+            "hash_size": 24,
+            "margin": 2.0,
+            "batch_size": 150,
+            "total_epoch_count": 100,
+            "number_of_epochs_per_decay": 150.0,
+            "weight_decay_factor": 5.0e-5,
+            "learning_rate": 0.016,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "loss": "loss_spring",
+            "hash_size": 24,
+            "margin": 2.0,
+            "batch_size": 150,
+            "total_epoch_count": 100,
+            "number_of_epochs_per_decay": 150.0,
+            "weight_decay_factor": 5.0e-5,
+            "learning_rate": 0.014,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "loss": "loss_spring",
+            "hash_size": 24,
+            "margin": 2.0,
+            "batch_size": 150,
+            "total_epoch_count": 100,
+            "number_of_epochs_per_decay": 150.0,
+            "weight_decay_factor": 5.0e-5,
+            "learning_rate": 0.012,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
         # {
         #     "loss": "loss_accv",
         #     "hash_size": 24,
