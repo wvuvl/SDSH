4843353c8185ce01ed77eecbace1465cb5443f09
Podgorskiy <stanislav@podgorskiy.com>
2017-11-06 13:44:27 -0500

    Turned of PCA init

--- constructor.py
+++ constructor.py
@@ -37,35 +37,35 @@ def net(batch_size, hash_size, margin=0, weight_decay_factor=0, loss_func=None):
     # fcw = tf.Variable(weights, name='fc8_custom/weights', dtype='float32')
 
     #
-    try:
-        mean = np.load("temp/mean.npy")
-        #
-        # V = np.load("temp/V.npy")
-        W = np.load("temp/W.npy")
-        # print(np.linalg.det(V))
-        # s = np.load("temp/s.npy")
-        # Is = 1.0 / np.diag(s[:24])
-        # Is = np.diagflat(Is)
-        w = W#np.matmul(V.T[:, :24], Is)
-        b = np.matmul(-mean, w)
-        #
-        fcw = tf.Variable(w, name='fc8_custom/weights', dtype='float32')
-        fcb = tf.Variable(b, name='fc8_custom/biases', dtype='float32')
+    # try:
+    #     mean = np.load("temp/mean.npy")
+    #     #
+    #     # V = np.load("temp/V.npy")
+    #     W = np.load("temp/W.npy")
+    #     # print(np.linalg.det(V))
+    #     # s = np.load("temp/s.npy")
+    #     # Is = 1.0 / np.diag(s[:24])
+    #     # Is = np.diagflat(Is)
+    #     w = W#np.matmul(V.T[:, :24], Is)
+    #     b = np.matmul(-mean, w)
+    #     #
+    #     fcw = tf.Variable(w, name='fc8_custom/weights', dtype='float32')
+    #     fcb = tf.Variable(b, name='fc8_custom/biases', dtype='float32')
+    #
+    # except IOError:
+    #     print("!"*80)
+    #     print("!"*80)
+    #     print("Warning!!! Not using SVD init!!!")
+    #     print("!"*80)
+    #     print("!"*80)
 
-    except IOError:
-        print("!"*80)
-        print("!"*80)
-        print("Warning!!! Not using SVD init!!!")
-        print("!"*80)
-        print("!"*80)
+    fcw = tf.get_variable(name='fc8_custom/weights', shape=[4096, hash_size],
+                          initializer=tf.truncated_normal_initializer(stddev=0.01, dtype=tf.float32),
+                          dtype=tf.float32)
 
-        fcw = tf.get_variable(name='fc8_custom/weights', shape=[4096, hash_size],
-                              initializer=tf.truncated_normal_initializer(stddev=0.01, dtype=tf.float32),
-                              dtype=tf.float32)
-
-        fcb = tf.get_variable(name='fc8_custom/biases', shape=[hash_size],
-                              initializer=tf.truncated_normal_initializer(stddev=0.01, dtype=tf.float32),
-                              dtype=tf.float32)
+    fcb = tf.get_variable(name='fc8_custom/biases', shape=[hash_size],
+                          initializer=tf.truncated_normal_initializer(stddev=0.01, dtype=tf.float32),
+                          dtype=tf.float32)
 
     model.weight_decay_losses.append(tf.abs(tf.reduce_mean(tf.reduce_sum(tf.square(fcw), 0)) - 1.0))
 
