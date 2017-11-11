10abf67e7ca28127a44b7a6bf15596e6f9ecc6bf
Podgorskiy <stanislav@podgorskiy.com>
2017-11-11 16:44:51 -0500

    schedule for cifar reduced

--- schedule.py
+++ schedule.py
@@ -96,17 +96,83 @@ if __name__ == '__main__':
             "learning_rate": 0.07,
             "learning_rate_decay_factor": 2.0 / 3.0
         },
-        # {
-        #     "loss": "loss_accv",
-        #     "hash_size": 24,
-        #     "margin": 12,
-        #     "batch_size": 150,
-        #     "total_epoch_count": 100,
-        #     "number_of_epochs_per_decay": 20.0,
-        #     "weight_decay_factor": 0.0011,
-        #     "learning_rate": 0.000225,
-        #     "learning_rate_decay_factor": 2.0 / 3.0
-        # },
+        {
+            "loss": "loss_accv_mod",
+            "hash_size": 24,
+            "margin": 0.5,
+            "batch_size": 100,
+            "total_epoch_count": 120,
+            "number_of_epochs_per_decay": 100.0,
+            "weight_decay_factor": 0.0002,
+            "learning_rate": 0.05,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "loss": "loss_accv_mod",
+            "hash_size": 16,
+            "margin": 0.5,
+            "batch_size": 100,
+            "total_epoch_count": 120,
+            "number_of_epochs_per_decay": 100.0,
+            "weight_decay_factor": 0.0002,
+            "learning_rate": 0.05,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "loss": "loss_accv_mod",
+            "hash_size": 32,
+            "margin": 0.5,
+            "batch_size": 100,
+            "total_epoch_count": 120,
+            "number_of_epochs_per_decay": 100.0,
+            "weight_decay_factor": 0.0002,
+            "learning_rate": 0.05,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "loss": "loss_accv_mod",
+            "hash_size": 12,
+            "margin": 0.5,
+            "batch_size": 100,
+            "total_epoch_count": 120,
+            "number_of_epochs_per_decay": 100.0,
+            "weight_decay_factor": 0.0002,
+            "learning_rate": 0.05,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "loss": "loss_accv_mod",
+            "hash_size": 8,
+            "margin": 0.5,
+            "batch_size": 100,
+            "total_epoch_count": 120,
+            "number_of_epochs_per_decay": 100.0,
+            "weight_decay_factor": 0.0002,
+            "learning_rate": 0.05,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "loss": "loss_accv_mod",
+            "hash_size": 4,
+            "margin": 0.5,
+            "batch_size": 100,
+            "total_epoch_count": 120,
+            "number_of_epochs_per_decay": 100.0,
+            "weight_decay_factor": 0.0002,
+            "learning_rate": 0.05,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "loss": "loss_accv_mod",
+            "hash_size": 48,
+            "margin": 0.5,
+            "batch_size": 100,
+            "total_epoch_count": 120,
+            "number_of_epochs_per_decay": 100.0,
+            "weight_decay_factor": 0.0002,
+            "learning_rate": 0.05,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
         # {
         #     "loss": "loss_accv",
         #     "hash_size": 24,
