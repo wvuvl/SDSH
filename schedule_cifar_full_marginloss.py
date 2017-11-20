2603f35286c34563999dbe17d77865b18521b727
Podgorskiy <stanislav@podgorskiy.com>
2017-11-20 13:30:20 -0500

    Added cifar full margin loss

---
+++ schedule_cifar_full_marginloss.py
@@ -0,0 +1,91 @@
+from train import Train
+
+if __name__ == '__main__':
+
+    path = "experiments"
+
+    train = Train()
+
+    experiments = [
+        # CIFAR10
+        {
+            "loss": "loss_triplet",
+            "hash_size": 24,
+            "margin": 2.0,
+            "batch_size": 150,
+            "total_epoch_count": 50,
+            "number_of_epochs_per_decay": 10.0,
+            "weight_decay_factor": 0.0002,
+            "learning_rate": 0.07,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "loss": "loss_triplet",
+            "hash_size": 24,
+            "margin": 2.0,
+            "batch_size": 150,
+            "total_epoch_count": 50,
+            "number_of_epochs_per_decay": 10.0,
+            "weight_decay_factor": 0.001,
+            "learning_rate": 0.07 / 4.0,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "loss": "loss_triplet",
+            "hash_size": 16,
+            "margin": 2.0,
+            "batch_size": 150,
+            "total_epoch_count": 50,
+            "number_of_epochs_per_decay": 10.0,
+            "weight_decay_factor": 0.001,
+            "learning_rate": 0.07 / 4.0,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "loss": "loss_triplet",
+            "hash_size": 32,
+            "margin": 2.0,
+            "batch_size": 150,
+            "total_epoch_count": 50,
+            "number_of_epochs_per_decay": 10.0,
+            "weight_decay_factor": 0.001,
+            "learning_rate": 0.07 / 4.0,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "loss": "loss_triplet",
+            "hash_size": 48,
+            "margin": 2.0,
+            "batch_size": 150,
+            "total_epoch_count": 50,
+            "number_of_epochs_per_decay": 10.0,
+            "weight_decay_factor": 0.001,
+            "learning_rate": 0.07 / 4.0,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "loss": "loss_triplet",
+            "hash_size": 8,
+            "margin": 2.0,
+            "batch_size": 150,
+            "total_epoch_count": 50,
+            "number_of_epochs_per_decay": 10.0,
+            "weight_decay_factor": 0.001,
+            "learning_rate": 0.07 / 4.0,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "loss": "loss_triplet",
+            "hash_size": 12,
+            "margin": 2.0,
+            "batch_size": 150,
+            "total_epoch_count": 50,
+            "number_of_epochs_per_decay": 10.0,
+            "weight_decay_factor": 0.001,
+            "learning_rate": 0.07 / 4.0,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+    ]
+
+    for e in experiments:
+        train.run(path, e)
