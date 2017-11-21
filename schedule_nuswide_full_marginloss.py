c3dbd39a9efaa47473d656e3e6fb878430f0af10
Podgorskiy <stanislav@podgorskiy.com>
2017-11-20 23:54:24 -0500

    nuswide for margin loss

---
+++ schedule_nuswide_full_marginloss.py
@@ -0,0 +1,86 @@
+from train import Train
+
+if __name__ == '__main__':
+
+    path = "experiments"
+
+    train = Train()
+
+    experiments = [
+        # NUS_WIDE 2100._
+        {
+            "dataset": "nus2100._",
+            "loss": "loss_triplet",
+            "hash_size": 24,
+            "margin": 2.0,
+            "batch_size": 150,
+            "total_epoch_count": 28,
+            "number_of_epochs_per_decay": 30,
+            "weight_decay_factor": 0.001,
+            "learning_rate": 0.07 / 4.0,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "dataset": "nus2100._",
+            "loss": "loss_triplet",
+            "hash_size": 16,
+            "margin": 2.0,
+            "batch_size": 150,
+            "total_epoch_count": 28,
+            "number_of_epochs_per_decay": 30,
+            "weight_decay_factor": 0.001,
+            "learning_rate": 0.07 / 4.0,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "dataset": "nus2100._",
+            "loss": "loss_triplet",
+            "hash_size": 32,
+            "margin": 2.0,
+            "batch_size": 150,
+            "total_epoch_count": 28,
+            "number_of_epochs_per_decay": 30,
+            "weight_decay_factor": 0.001,
+            "learning_rate": 0.07 / 4.0,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "dataset": "nus2100._",
+            "loss": "loss_triplet",
+            "hash_size": 12,
+            "margin": 2.0,
+            "batch_size": 150,
+            "total_epoch_count": 28,
+            "number_of_epochs_per_decay": 30,
+            "weight_decay_factor": 0.001,
+            "learning_rate": 0.07 / 4.0,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "dataset": "nus2100._",
+            "loss": "loss_triplet",
+            "hash_size": 8,
+            "margin": 2.0,
+            "batch_size": 150,
+            "total_epoch_count": 28,
+            "number_of_epochs_per_decay": 30,
+            "weight_decay_factor": 0.001,
+            "learning_rate": 0.07 / 4.0,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+        {
+            "dataset": "nus2100._",
+            "loss": "loss_triplet",
+            "hash_size": 48,
+            "margin": 2.0,
+            "batch_size": 150,
+            "total_epoch_count": 28,
+            "number_of_epochs_per_decay": 30,
+            "weight_decay_factor": 0.001,
+            "learning_rate": 0.07 / 4.0,
+            "learning_rate_decay_factor": 2.0 / 3.0
+        },
+    ]
+
+    for e in experiments:
+        train.run(path, e)
